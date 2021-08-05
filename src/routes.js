97fc1b353f78c8f33dc62027a16df187df949e80
Guilherme Fontanella <gui.jmf@hotmail.com>
2021-08-05 17:52:27 -0300

    estudos api

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const noteController = require('./controllers/NoteController');
+
+router.get('/ping', noteController.ping);
+
+router.get('/notes', noteController.all);
+router.get('/note/:id', noteController.one);
+router.post('/note', noteController.new);
+router.put('/note/:id', noteController.edit);
+router.delete('/note/:id', noteController.delete);
+
+
+module.exports = router;
